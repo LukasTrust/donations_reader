ab6b1a9d0096504db11d605862247c4778bedccb
LukasTrust <Dingenses22@gmx.de>
2023-10-04 21:28:21 +0200

    changed the database saving from row to column based

--- DonationsProject/Classes/Database/DBConnector.cs
+++ DonationsProject/Classes/Database/DBConnector.cs
@@ -8,6 +8,7 @@ using System.Data.SqlClient;
 using DonationsProject.Classes.Objects;
 using OpenQA.Selenium;
 using System.Windows.Media;
+using System.Globalization;
 
 namespace DonationsProject.Classes.Database
 {
@@ -46,92 +47,190 @@ namespace DonationsProject.Classes.Database
             {
                 Connect();
             }
-            foreach (Donation donation in donations)
-            {
-                // Insert into Dates table
-                int dateReceiptId = await InsertDate(donation.ReceiptDate);
+            await ClearAllTables();
+            // Insert into Dates table
+            List<int> dateReceiptId = await InsertDonationDate(donations.Select(x => x.ReceiptDate).ToList());
 
-                int dateReportId = await InsertDate(donation.ReportLink);
+            List<int> dateReportId = await InsertReportDate(donations.Select(x => x.ReportLink).ToList());
 
-                // Insert into Donors table
-                int donorId = await InsertDonor(donation.Donor);
+            // Insert into Donors table
+            List<int> donorId = await InsertDonor(donations.Select(x => x.Donor).ToList());
 
-                // Insert into Parties table
-                int partyId = await InsertParty(donation.Party);
+            // Insert into Parties table
+            List<int> partyId = await InsertParty(donations.Select(x => x.Party).ToList());
 
-                // Insert into Donations table
-                await InsertDonation(dateReceiptId, dateReportId, donorId, partyId, donation.Amount);
-            }
+            List<int> amountId = await InsertAmount(donations.Select(x => x.Amount).ToList());
 
+            // Insert into Donations table
+            await InsertDonation(dateReceiptId, dateReportId, donorId, partyId, amountId);
         }
 
-        private async Task<int> Insert(string tableName, string columnName, string parameterName, object value)
+        private async Task ClearAllTables()
         {
-            string insertQuery = $"INSERT INTO {tableName} ({columnName}) OUTPUT INSERTED.Id VALUES (@{parameterName})";
-
-            using (SqlCommand command = new SqlCommand(insertQuery, connection))
+            if (connection == null)
             {
-                await Task.Run(() =>
-                {
-                    command.Parameters.AddWithValue("@" + parameterName, value);
-                });
+                Connect();
+            }
 
-                try
+            string[] tableNames = { "Donations", "Parties", "DonationDates", "ReportDates", "Amounts", "Doners" };
+
+            foreach (var tableName in tableNames)
+            {
+                string deleteQuery = $"DELETE FROM {tableName}";
+                using (SqlCommand command = new SqlCommand(deleteQuery, connection))
                 {
-                    return (int)command.ExecuteScalar();
+                    await command.ExecuteNonQueryAsync();
                 }
-                catch (SqlException ex)
+            }
+        }
+
+        private async Task<List<int>> Insert(string tableName, string columnName, List<object> values)
+        {
+            List<int> ids = new List<int>();
+            List<object> distinctValues = values.Distinct().ToList();
+            List<(int Id, object Value)> existingKeys = await ReadDataFromDB(tableName, columnName);
+            List<object> toInsert = new List<object>();
+
+            foreach (object value in distinctValues)
+            {
+                if (!existingKeys.Any(x => x.Value.Equals(value)))
                 {
-                    if (ex.Number == 2627) // Unique constraint violation error number
+                    if (value is double)
                     {
-                        // Handle unique constraint violation error by returning the existing key
-                        string selectExistingKeyQuery = $"SELECT Id FROM {tableName} WHERE {columnName} = @{parameterName}";
-                        using (SqlCommand selectCommand = new SqlCommand(selectExistingKeyQuery, connection))
-                        {
-                            selectCommand.Parameters.AddWithValue("@" + parameterName, value);
-                            return (int)selectCommand.ExecuteScalar();
-                        }
+                        string valueString = ((double)value).ToString();
+                        valueString = valueString.Replace(",", ".");
+                        toInsert.Add(valueString);
                     }
                     else
                     {
-                        // Handle other SQL exceptions
-                        Console.WriteLine("An error occurred: " + ex.Message);
-                        throw; // Rethrow the exception for other types of errors
+                        toInsert.Add(value.ToString());
                     }
                 }
             }
-        }
 
-
-        private async Task<int> InsertDate(DateTime date)
-        {
-            return await Insert("Dates", "Date", "Date", date);
-        }
-
-        private async Task<int> InsertDonor(string donor)
-        {
-            return await Insert("Doners", "Name", "Name", donor);
-        }
-
-        private async Task<int> InsertParty(string party)
-        {
-            return await Insert("Partys", "Name", "Name", party);
-        }
-
-        private async Task InsertDonation(int dateReceiptId, int dateReportId, int donorId, int partyId, double amount)
-        {
-            string insertDonationQuery = string.Format(InsertQuery, "Donations", "FK_DonationDate, FK_ReportDate, FK_Doner, FK_Party, Amount", "Id", "@FK_DonationDate, @FK_ReportDate, @FK_Doner, @FK_Party, @Amount");
-            SqlCommand command = new SqlCommand(insertDonationQuery, connection);
-            await Task.Run(() =>
+            if (toInsert.Count != 0)
             {
-                command.Parameters.AddWithValue("@FK_DonationDate", dateReceiptId);
-                command.Parameters.AddWithValue("@FK_ReportDate", dateReportId);
-                command.Parameters.AddWithValue("@FK_Doner", donorId);
-                command.Parameters.AddWithValue("@FK_Party", partyId);
-                command.Parameters.AddWithValue("@Amount", amount);
+                string insertQuery = $"INSERT INTO {tableName} ({columnName}) VALUES ";
 
-                command.ExecuteNonQuery();
-            });
+                for (int i = 0; i < toInsert.Count; i++)
+                {
+                    insertQuery += $"('{toInsert[i]}')";
+                    if (i < toInsert.Count - 1)
+                        insertQuery += ",";
+                }
+
+                using (SqlCommand insertCommand = new SqlCommand(insertQuery, connection))
+                {
+                    await insertCommand.ExecuteNonQueryAsync();
+                }
+
+                existingKeys = await ReadDataFromDB(tableName, columnName);
+            }
+
+            foreach (object value in values)
+            {
+                foreach (var existingValue in existingKeys)
+                {
+                    if (existingValue.Value.ToString() == value.ToString())
+                    {
+                        ids.Add(existingValue.Id);
+                        break;
+                    }
+                }
+            }
+            return ids;
+        }
+
+        private async Task<List<(int Id, object Value)>> ReadDataFromDB(string tableName, string columnName)
+        {
+            List<(int Id, object Value)> existingKeys = new List<(int, object Value)>();
+            string selectExistingKeyQuery = $"SELECT Id, {columnName} FROM {tableName}";
+
+            using (SqlCommand command = new SqlCommand(selectExistingKeyQuery, connection))
+            using (SqlDataReader reader = await command.ExecuteReaderAsync())
+            {
+                while (await reader.ReadAsync())
+                {
+                    int id = reader.GetInt32(0);  // Assuming Id is in the first column
+                    object value = reader.GetValue(1);  // Assuming the value is in the second column
+
+                    existingKeys.Add((id, value));
+                }
+            }
+            return existingKeys;
+        }
+
+
+        private async Task<List<int>> InsertDonationDate(List<DateTime> dates)
+        {
+            List<object> list = dates.Cast<object>().ToList();
+            return await Insert("DonationDates", "Date", list);
+        }
+
+        private async Task<List<int>> InsertReportDate(List<DateTime> dates)
+        {
+            List<object> list = dates.Cast<object>().ToList();
+            return await Insert("ReportDates", "Date", list);
+        }
+
+        private async Task<List<int>> InsertDonor(List<string> donors)
+        {
+            List<object> list = donors.Cast<object>().ToList();
+            return await Insert("Doners", "Name", list);
+        }
+
+        private async Task<List<int>> InsertParty(List<string> parties)
+        {
+            List<object> list = parties.Cast<object>().ToList();
+            return await Insert("Parties", "Name", list);
+        }
+
+        private async Task<List<int>> InsertAmount(List<double> amounts)
+        {
+            List<object> list = amounts.Cast<object>().ToList();
+            return await Insert("Amounts", "Amount", list);
+        }
+
+        private async Task InsertDonation(List<int> dateReceiptIds, List<int> dateReportIds, List<int> donorIds, List<int> partyIds, List<int> amounts)
+        {
+            int itemCount = dateReceiptIds.Count;
+            if (itemCount != dateReportIds.Count || itemCount != donorIds.Count || itemCount != partyIds.Count || itemCount != amounts.Count)
+            {
+                throw new ArgumentException("Input lists must have the same count.");
+            }
+
+            int batchSize = 100;
+
+            int numBatches = (int)Math.Ceiling((double)itemCount / batchSize);
+
+            for (int batchIndex = 0; batchIndex < numBatches; batchIndex++)
+            {
+                int startIndex = batchIndex * batchSize;
+                int endIndex = Math.Min((batchIndex + 1) * batchSize, itemCount);
+
+                StringBuilder insertDonationQuery = new StringBuilder("INSERT INTO Donations (FK_DonationDate, FK_ReportDate, FK_Doner, FK_Party, FK_Amount) VALUES ");
+
+                for (int i = startIndex; i < endIndex; i++)
+                {
+                    insertDonationQuery.Append($"(@FK_DonationDate{i}, @FK_ReportDate{i}, @FK_Doner{i}, @FK_Party{i}, @FK_Amount{i})");
+                    if (i < endIndex - 1)
+                        insertDonationQuery.Append(",");
+                }
+
+                using (SqlCommand command = new SqlCommand(insertDonationQuery.ToString(), connection))
+                {
+                    for (int i = startIndex; i < endIndex; i++)
+                    {
+                        command.Parameters.AddWithValue($"@FK_DonationDate{i}", dateReceiptIds[i]);
+                        command.Parameters.AddWithValue($"@FK_ReportDate{i}", dateReportIds[i]);
+                        command.Parameters.AddWithValue($"@FK_Doner{i}", donorIds[i]);
+                        command.Parameters.AddWithValue($"@FK_Party{i}", partyIds[i]);
+                        command.Parameters.AddWithValue($"@FK_Amount{i}", amounts[i]);
+                    }
+
+                    await command.ExecuteNonQueryAsync();
+                }
+            }
         }
     }
 }
