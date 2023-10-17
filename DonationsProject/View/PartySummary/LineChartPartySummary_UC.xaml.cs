73f822b14b30e04daa68930a53393624dc50101e
LukasTrust <Dingenses22@gmx.de>
2023-10-17 20:41:55 +0200

    Added sections to the LineView so the election years a visable

--- DonationsProject/View/PartySummary/LineChartPartySummary_UC.xaml.cs
+++ DonationsProject/View/PartySummary/LineChartPartySummary_UC.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LiveCharts.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,10 +21,35 @@ namespace DonationsProject.View.PartySummary
     /// </summary>
     public partial class LineChartPartySummary_UC : UserControl
     {
+        public List<int> ElectionYears = new List<int>() { 2002, 2005, 2009, 2013, 2017, 2021 };
+
         public LineChartPartySummary_UC()
         {
             InitializeComponent();
             DataContext = ViewModel.LineChartPartySummary_VM.Instance;
+            Func<double, string> formatFunc = (x) => string.Format("{0:N}", x);
+            YAxis.LabelFormatter = formatFunc;
+        }
+
+        private void XAxis_TargetUpdated(object sender, DataTransferEventArgs e)
+        {
+            if (XAxis == null ||XAxis.Labels == null)
+                return;
+            List<string> lables = XAxis.Labels.ToList();
+            for (int i = 0; i < lables.Count; i++)
+            {
+                if (ElectionYears.Contains(int.Parse(lables[i])))
+                {
+                    AxisSection section = new AxisSection
+                    {
+                        Value = i,
+                        Stroke = Brushes.Red,
+                        StrokeThickness = 2,
+                    };
+                    XAxis.Sections.Add(section);
+                }
+            }
+            UpdateLayout();
         }
     }
 }
